2a031ea36b777826154c1ac0a30a326ba5a9139e
Max Omenes <ukhin.maxim@gmail.com>
2024-03-25 19:42:08 +0300

    add, fix: lose check

--- game.py
+++ game.py
@@ -28,34 +28,45 @@ class Game:
         can = []
         table: list[list[CellType]] = self.table.table.copy()
         if row < len(table) - 2:  # down check
-            if (table[row + 2][col] == CellType.EMPTY and
+            if ((table[row + 2][col] == CellType.EMPTY or
+                    table[row + 2][col] == CellType.CAN) and
                     table[row + 1][col] == CellType.BLOCK):
-                table[row + 2][col] = CellType.CAN
                 can.append((row + 2, col))
 
         if row > 1:  # up check
-            if (table[row - 2][col] == CellType.EMPTY and
+            if ((table[row - 2][col] == CellType.EMPTY or
+                    table[row - 2][col] == CellType.CAN) and
                     table[row - 1][col] == CellType.BLOCK):
-                table[row - 2][col] = CellType.CAN
                 can.append((row - 2, col))
 
         if col < len(table[0]) - 2: # right check
-            if (table[row][col + 2] == CellType.EMPTY and
+            if ((table[row][col + 2] == CellType.EMPTY or
+                    table[row][col + 2] == CellType.CAN) and
                     table[row][col + 1] == CellType.BLOCK):
-                table[row][col + 2] = CellType.CAN
                 can.append((row, col + 2))
 
         if col > 1:  # left check
-            if (table[row][col - 2] == CellType.EMPTY and
+            if ((table[row][col - 2] == CellType.EMPTY or
+                    table[row][col - 2] == CellType.CAN) and
                     table[row][col - 1] == CellType.BLOCK):
-                table[row][col - 2] = CellType.CAN
                 can.append((row, col - 2))
 
         return can
 
-    def event(self, row, col):
+    def game_status(self) -> bool:
         if self.status != GameStatus.PLAYING:
-            return
+            return False
+        if self.check_win():
+            self.status = GameStatus.WIN
+            print("You win!")
+            return True
+        if self.check_lose():
+            self.status = GameStatus.LOSE
+            print("Looser!")
+
+            return True
+
+    def event(self, row, col):
 
         clicked_cell = self.table.table[row][col]
 
@@ -68,6 +79,8 @@ class Game:
                 can = self.calculate_can(row, col)
                 self.can_row = [x for x, y in can]
                 self.can_col = [y for x, y in can]
+                for i in range(len(can)):
+                    self.table.add_can(self.can_row[i], self.can_col[i])
             case CellType.CAN:
                 empty_row = (row + self.table.selected_row) // 2
                 empty_col = (col + self.table.selected_col) // 2
@@ -77,18 +90,14 @@ class Game:
                 selected_row, selected_col = self.table.get_selected()
                 self.table.remove_selected()
                 self.table.set_empty(selected_row, selected_col)
-
             case CellType.SELECTED:
                 self.table.remove_selected()
                 self.remove_can()
 
         self.table.show_table()
 
-        if self.check_win():  # check if the player has won
-            self.status = GameStatus.WIN
-
-        # if self.check_lose():
-        #     self.status = GameStatus.LOSE
+        if self.game_status():
+            return
 
     def check_win(self):
         count = 0
@@ -98,3 +107,12 @@ class Game:
                     count += 1
 
         return count == 1
+
+    def check_lose(self):
+        for i in range(len(self.table.table)):
+            for j in range(len(self.table.table[0])):
+                if (self.table.table[i][j] == CellType.BLOCK or
+                        self.table.table[i][j] == CellType.SELECTED):
+                    if self.calculate_can(i, j):
+                        return False
+        return True
